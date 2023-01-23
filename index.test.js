15fd3eefd835754d4d82e501e307cda99bdca772
frami <i@2fd.me>
2023-01-23 00:34:20 -0300

    chore: run build and run test

---
+++ index.test.js
@@ -0,0 +1,14 @@
+const { Canvas } = require('skia-canvas')
+
+const CONTEXT_FONT_DEFAULT_VALUE = '10px sans-serif'
+describe('skia-canvas context', () => {
+  test('should be able to edit the context', () => {
+    const canvas = new Canvas(100, 100)
+    const context = canvas.getContext('2d')
+    expect(context.font).toBe(CONTEXT_FONT_DEFAULT_VALUE)
+
+    context.font = '600 20px Times'
+    expect(context.font).not.toBe(CONTEXT_FONT_DEFAULT_VALUE)
+    expect(context.font).toBe('normal 600 20px/24px Times')
+  })
+})
